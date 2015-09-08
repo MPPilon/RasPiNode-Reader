eb80068d3767801fa9a54c8e7e97f0319922a33a
Jeff Codling <circuitjc@gmail.com>
2015-09-08 16:25:29 -0400

    Charting working with seperate charts for each variable

--- wsindex.js
+++ wsindex.js
@@ -4,25 +4,236 @@
 
 // Data collection
 
-//var chartData = [
-//    { 'id': '#sonic', 'values': [] },
-//    { 'id': '#pumpspeed', 'values': [] },
-//    { 'id': '#pressure', values: []}
-//];
+Chart.defaults.global = {
+    // Boolean - Whether to animate the chart
+    animation: false,
 
-var chartData = {
-    labels: [],
+    // Number - Number of animation steps
+    animationSteps: 60,
+
+    // String - Animation easing effect
+    // Possible effects are:
+    // [easeInOutQuart, linear, easeOutBounce, easeInBack, easeInOutQuad,
+    //  easeOutQuart, easeOutQuad, easeInOutBounce, easeOutSine, easeInOutCubic,
+    //  easeInExpo, easeInOutBack, easeInCirc, easeInOutElastic, easeOutBack,
+    //  easeInQuad, easeInOutExpo, easeInQuart, easeOutQuint, easeInOutCirc,
+    //  easeInSine, easeOutExpo, easeOutCirc, easeOutCubic, easeInQuint,
+    //  easeInElastic, easeInOutSine, easeInOutQuint, easeInBounce,
+    //  easeOutElastic, easeInCubic]
+    animationEasing: "easeOutQuart",
+
+    // Boolean - If we should show the scale at all
+    showScale: true,
+
+    // Boolean - If we want to override with a hard coded scale
+    scaleOverride: false,
+
+    // ** Required if scaleOverride is true **
+    // Number - The number of steps in a hard coded scale
+    scaleSteps: null,
+    // Number - The value jump in the hard coded scale
+    scaleStepWidth: null,
+    // Number - The scale starting value
+    scaleStartValue: null,
+
+    // String - Colour of the scale line
+    scaleLineColor: "rgba(0,0,0,.1)",
+
+    // Number - Pixel width of the scale line
+    scaleLineWidth: 1,
+
+    // Boolean - Whether to show labels on the scale
+    scaleShowLabels: true,
+
+    // Interpolated JS string - can access value
+    scaleLabel: "<%=value%>",
+
+    // Boolean - Whether the scale should stick to integers, not floats even if drawing space is there
+    scaleIntegersOnly: true,
+
+    // Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+    scaleBeginAtZero: false,
+
+    // String - Scale label font declaration for the scale label
+    scaleFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+
+    // Number - Scale label font size in pixels
+    scaleFontSize: 12,
+
+    // String - Scale label font weight style
+    scaleFontStyle: "normal",
+
+    // String - Scale label font colour
+    scaleFontColor: "#666",
+
+    // Boolean - whether or not the chart should be responsive and resize when the browser does.
+    responsive: false,
+
+    // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+    maintainAspectRatio: true,
+
+    // Boolean - Determines whether to draw tooltips on the canvas or not
+    showTooltips: true,
+
+    // Function - Determines whether to execute the customTooltips function instead of drawing the built in tooltips (See [Advanced - External Tooltips](#advanced-usage-custom-tooltips))
+    customTooltips: false,
+
+    // Array - Array of string names to attach tooltip events
+    tooltipEvents: ["mousemove", "touchstart", "touchmove"],
+
+    // String - Tooltip background colour
+    tooltipFillColor: "rgba(0,0,0,0.8)",
+
+    // String - Tooltip label font declaration for the scale label
+    tooltipFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+
+    // Number - Tooltip label font size in pixels
+    tooltipFontSize: 14,
+
+    // String - Tooltip font weight style
+    tooltipFontStyle: "normal",
+
+    // String - Tooltip label font colour
+    tooltipFontColor: "#fff",
+
+    // String - Tooltip title font declaration for the scale label
+    tooltipTitleFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+
+    // Number - Tooltip title font size in pixels
+    tooltipTitleFontSize: 14,
+
+    // String - Tooltip title font weight style
+    tooltipTitleFontStyle: "bold",
+
+    // String - Tooltip title font colour
+    tooltipTitleFontColor: "#fff",
+
+    // Number - pixel width of padding around tooltip text
+    tooltipYPadding: 6,
+
+    // Number - pixel width of padding around tooltip text
+    tooltipXPadding: 6,
+
+    // Number - Size of the caret on the tooltip
+    tooltipCaretSize: 8,
+
+    // Number - Pixel radius of the tooltip border
+    tooltipCornerRadius: 6,
+
+    // Number - Pixel offset from point x to tooltip edge
+    tooltipXOffset: 10,
+
+    // String - Template string for single tooltips
+    tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>",
+
+    // String - Template string for multiple tooltips
+    multiTooltipTemplate: "<%= value %>",
+
+    // Function - Will fire on animation progression.
+    onAnimationProgress: function(){},
+
+    // Function - Will fire on animation completion.
+    onAnimationComplete: function(){}
+};
+
+// Maximum number of datapoints to store
+var maxDataToStore = 30,
+    i,
+    labelsArray = ['nd'],
+    dataArray = [],
+    dataArray2 = [],
+    dataArray3 = [];
+
+for(i = 0; i < maxDataToStore; i++) {
+    labelsArray.push('');
+    dataArray.push(0);
+    dataArray2.push(0);
+    dataArray3.push(0);
+}
+
+var sonicChart = {
+    labels: labelsArray,
     datasets: [
         {
             label: "Sonic",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: dataArray
+        }
+    ]
+};
+
+var pumpChart = {
+    labels: labelsArray,
+    datasets: [
+        {
+            label: "Pump Speed",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: dataArray2
+        }
+    ]
+};
+
+var pressureChart = {
+    labels: labelsArray,
+    datasets: [
+        {
+            label: "Pressure",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: dataArray3
+        }
+    ]
+};
+
+var chartData = {
+    labels: labelsArray,
+    datasets: [
+        {
             id: "#sonic",
+            label: "Sonic",
             fillColor: "rgba(220,220,220,0.2)",
             strokeColor: "rgba(220,220,220,1)",
             pointColor: "rgba(220,220,220,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(220,220,220,1)",
-            data: []
+            data: dataArray
+        },
+        {
+            id: "#pumpspeed",
+            label: "Pumpspeed",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: dataArray2
+        },
+        {
+            id: "#pressure",
+            label: "Pressure",
+            fillColor: "rgba(187,151,205,0.2)",
+            strokeColor: "rgba(187,151,205,1)",
+            pointColor: "rgba(187,151,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(187,151,205,1)",
+            data: dataArray3
         }
     ]
 };
@@ -75,12 +286,19 @@ var chartOptions = {
     legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
 };
 
-var ctx = document.getElementById('chart');
-var ctx2 = ctx.getContext("2d");
-var chrt = new Chart(ctx2).Line(chartData, chartOptions);
+var ctxSonic = document.getElementById('sonicChart');
+var ctxPump = document.getElementById('pumpChart');
+var ctxPressure = document.getElementById('pressureChart');
+var ctx2Sonic = ctxSonic.getContext("2d");
+var ctx2Pump = ctxPump.getContext("2d");
+var ctx2Pressure = ctxPressure.getContext("2d");
+var sonicChrt = null,
+    pumpChrt = null,
+    pressureChrt = null;
 
-// Maximum number of datapoints to store
-var maxDataToStore = 5;
+sonicChrt = new Chart(ctx2Sonic).Line(sonicChart, chartOptions);
+pumpChrt = new Chart(ctx2Pump).Line(pumpChart, chartOptions);
+pressureChrt = new Chart(ctx2Pressure).Line(pressureChart, chartOptions);
 
 // getTimestamp - Returns the current time in hours, minutes, seconds and milliseconds to 3 decimal places
 function getTimestamp() {
@@ -144,24 +362,40 @@ var new_connection = function() {
 
         // Track charting data
 
-        tempValues = [];
+        //tempValues = [];
 
-        chartData.datasets.forEach(function(v) {
-            if (v.id == content.id) {
-                tempValues = v.data;
-                tempValues.push(content.reading);
-                chartData.labels.push(getTimestamp());
-                if (chartData.labels.length > maxDataToStore) {
-                    chartData.labels.shift();
-                }
-                if (tempValues.length > maxDataToStore) {
-                    tempValues.shift();
-                }
-                v.data = tempValues;
-            }
-        });
+        //chartData.datasets.forEach(function(v) {
+        //    if (v.id == content.id) {
+        //        tempValues = v.data;
+        //        tempValues.push(content.reading);
+        //        chartData.labels.push(getTimestamp());
+        //        if (chartData.labels.length > maxDataToStore) {
+        //            chartData.labels.shift();
+        //        }
+        //        if (tempValues.length > maxDataToStore) {
+        //            tempValues.shift();
+        //        }
+        //        v.data = tempValues;
+        //    }
+        //});
 
-        chrt.update();
+        //chrt = new Chart(ctx2).Line(chartData, chartOptions);
+
+        if (content.id == '#sonic') {
+            //sonicChrt = new Chart(ctx2Sonic).Line(sonicChart, chartOptions);
+            sonicChrt.addData([content.reading], getTimestamp());
+            sonicChrt.removeData();
+        } else if (content.id == '#pumpspeed') {
+            //pumpChrt = new Chart(ctx2Sonic).Line(sonicChart, chartOptions);
+            pumpChrt.addData([content.reading], getTimestamp());
+            pumpChrt.removeData();
+        } else if (content.id == '#pressure') {
+            //pressureChrt = new Chart(ctx2Sonic).Line(sonicChart, chartOptions);
+            pressureChrt.addData([content.reading], getTimestamp());
+            pressureChrt.removeData();
+        }
+
+        //chrt.update();
 
         $('#content').val(function(i, text) {
             return getTimestamp()
